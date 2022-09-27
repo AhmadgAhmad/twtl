3d6417bcfc3806af502f773e357ff72445b5db50
ahmadgahmad <ahmadgh@bu.edu>
2022-09-27 11:31:13 -0400

    [First commit] RoIS function based based off STL-RRT*

--- twtl/twtl.py
+++ twtl/twtl.py
@@ -378,6 +378,55 @@ def robustness(formula,traj,time_traj,t1=None,t2=None):#t1=0,t2=None,shift = 0):
     else: 
         raise('You are not accounting for op:%d',formula.op)
 
+    
+    def rois(formula,traj,shift):
+        '''FIXME: call online monitor
+        assumes traj is sorted w.r.t. times
+        '''
+        assert len(traj[0]) == len(traj[1]), traj
+        assert shift >= 0.0
+        
+        if formula.op == Op.NOP:
+            rho_low, rho_high = [max,max] 
+        elif formula.op == Op.HOLD: # The base case
+            s, t = traj
+            if t[0] <= shift <= t[-1]:
+                s_i = [x[self.var_idx] for x in s]
+                x = np.interp(shift, t, s_i)
+                if self.rel == STL.LESS:
+                    rho = self.mu - x
+                else: # self.rel == STL.GREATER
+                    rho = x - self.mu
+                rho_low, rho_high = rho, rho
+            else:
+                rho_low, rho_high = -self.space.rho_max, self.space.rho_max
+        elif self.op in (STL.OR, STL.AND):
+            rho_low, rho_high = zip(*[f.rosi(traj, shift)
+                                                     for f in self.subformulae])
+            if self.op == STL.OR:
+                rho_low, rho_high = max(rho_low), max(rho_high)
+            else: # self.op == STL.AND
+                rho_low, rho_high = min(rho_low), min(rho_high)
+        elif self.op == STL.NOT:
+            rho_low, rho_high = self.subformula.rosi(traj)
+            rho_low, rho_high = -rho_high, -rho_low
+        elif self.op in (STL.EVENTUALLY, STL.ALWAYS):
+            times = set([t for t in traj[1] if self.low <= t <= self.high]
+                        + [self.low, self.high]) 
+            rho_low, rho_high = zip(*[self.subformula.rosi(traj, t)
+                                                                for t in times])
+            if self.op == STL.EVENTUALLY:
+                rho_low, rho_high = max(rho_low), max(rho_high)
+            else: # self.op == STL.ALWAYS
+                rho_low, rho_high = min(rho_low), min(rho_high)
+        elif self.op == STL.UNTIL:
+            raise NotImplementedError
+        else:
+            raise ValueError('Unknown operation, opcode: %d!', self.op)
+        
+        assert rho_low <= rho_high
+        return rho_low, rho_high
+    
     #------------------------------------------
     # ast = twtl_dfa.tree 
 
